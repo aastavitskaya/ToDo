9ec6ceb9308916abdd3233402b9ab5a7ba341f0c
Anastasia <aastavitskaya@gmail.com>
2023-11-11 01:27:35 +0400

    good 🌃 night

--- authapp/management/commands/createuser.py
+++ authapp/management/commands/createuser.py
@@ -1,9 +1,22 @@
 from django.core.management.base import BaseCommand
-from django.core.management import call_command
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
+
 
 class Command(BaseCommand):
-    help = 'Create super user and test users from fixture 001.json'
-
+    help = 'Create super user and test users'
+    
     def handle(self, *args, **options):
-        call_command('loaddata', '001.json')
+
+        User = get_user_model()
+        try:
+            User.objects.create_superuser('admin@mail.ru', 'admin')
         
+            for i in range(1, 3):
+                email = f'user{i}@user.ru'
+                password = f'1234{i}'
+                user = User.objects.create(email=email)
+                user.set_password(password)
+                user.save()
+        except IntegrityError:
+            print("🙌 Упс, пользователь с email {email} уже есть")
