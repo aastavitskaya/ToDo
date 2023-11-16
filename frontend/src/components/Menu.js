447d3c7359165b4ca96ce3ca9c90cb3beb1752e4
Anastasia <aastavitskaya@gmail.com>
2023-11-16 12:50:09 +0400

    Menu & Footer 🙌

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li>Главная</li>
+        <li>Пользователи</li>
+        <li>О нас</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
