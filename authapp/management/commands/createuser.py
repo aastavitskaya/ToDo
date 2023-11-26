006056a30de70c49881a813100ce6c55b55f3564
Anastasia <aastavitskaya@gmail.com>
2023-11-27 00:51:51 +0400

    happy mother's day👩‍👧‍👦

--- authapp/management/commands/createuser.py
+++ authapp/management/commands/createuser.py
@@ -5,18 +5,21 @@ from django.db import IntegrityError
 
 class Command(BaseCommand):
     help = 'Create super user and test users'
-    
+
     def handle(self, *args, **options):
 
         User = get_user_model()
         try:
             User.objects.create_superuser('admin@mail.ru', 'admin')
-        
+
             for i in range(1, 3):
                 email = f'user{i}@user.ru'
                 password = f'1234{i}'
-                user = User.objects.create(email=email)
-                user.set_password(password)
-                user.save()
+                try:
+                    user = User.objects.create(email=email)
+                    user.set_password(password)
+                    user.save()
+                except IntegrityError:
+                    print(f'🙌 Упс, пользователь с email {email} уже есть')
         except IntegrityError:
-            print("🙌 Упс, пользователь с email {email} уже есть")
+            print('🙌 Упс, superuser уже есть')
--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import HyperlinkedModelSerializer
 
 from authapp.models import CustomUser
 
 
 class CustomUserModelSerializer(HyperlinkedModelSerializer):
+
     class Meta:
         model = CustomUser
         fields = [
@@ -12,4 +14,8 @@ class CustomUserModelSerializer(HyperlinkedModelSerializer):
             "last_name",
             "email",
         ]
-        
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data["password"] = make_password(password)
+        return super().create(validated_data)
