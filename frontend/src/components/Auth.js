d282a079260b7635672452afe318baf613b72833
Anastasia <aastavitskaya@gmail.com>
2024-01-11 13:26:33 +0400

    Navigate to  🏆

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,43 +1,52 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { Navigate } from 'react-router-dom';
 
 const LoginForm = ({ getToken, setToken }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     getToken(email, password, setToken);
     setEmail('');
     setPassword('');
+    setIsLoggedIn(true); 
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Введите email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </Form.Group>
+    <>
+      {isLoggedIn ? (
+        <Navigate to="/projects" replace />
+      ) : (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Несуществующий email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Пароль</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Введите пароль"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Пароль</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Пароль на клингонском"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Войти
-      </Button>
-    </Form>
+          <Button variant="primary" type="submit">
+            Войти
+          </Button>
+        </Form>
+      )}
+    </>
   );
 };
 
