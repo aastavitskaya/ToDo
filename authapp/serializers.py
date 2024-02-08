d316d2aadbd6389e439844518d3e3bda3889b68b
Anastasia <aastavitskaya@gmail.com>
2024-02-08 14:44:01 +0400

    api docs вместо 💪

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -27,3 +27,19 @@ class CustomUserModelSerializer(HyperlinkedModelSerializer):
         if password:
             validated_data["password"] = make_password(password)
         return super().update(instance, validated_data)
+
+
+class NewCustomUserModelSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            "id",
+            "url",
+            "password",
+            "first_name",
+            "last_name",
+            "email",
+            "is_staff",
+            "is_superuser",
+        ]
