facea89a70519e21004ab8c8aa29853b45fb9cb2
Anastasia <aastavitskaya@gmail.com>
2024-03-06 01:13:12 +0400

    btn create 🍬 🌃 ❤️

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,6 +1,7 @@
-import { Link, useLocation, NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { deleteItem } from '../core/actions';
 import { PROJECTS_API } from '../core/consts';
+import React from "react";
 
 
 export default function ProjectItem ({ project, users, setProjects }) {
@@ -10,29 +11,28 @@ export default function ProjectItem ({ project, users, setProjects }) {
   };
 
   return (
-    <tr>
-      <td>{project.id}</td>
-      <td>
-        {location.pathname === "/projects" ? (
-          <Link to={`/project/${project.id}`}>{project.projectName}</Link>
-        ) : (
-          <td>{project.projectName}</td>
-        )}
-      </td>
-      <td>{project.linkToRepo}</td>
-      <td>{project.description}</td>
-      <td>{project.projectTeam.map(id => {
-          const contributor = users.find(user => user.id === id);
-          return (!!contributor) ? <p key={id}>{contributor.email}</p> : <p>Loading...</p>;
-      })}
-      </td>
-      {location.pathname === "/projects" && (
-        // Кнопка "Delete" только на странице "/projects"
-        <td>
-          <button onClick={handleDelete}>Delete</button>
-        </td>
-      )}
-      <NavLink to='/projects/create'>Create</NavLink>
-    </tr>
+        <tr>
+          <td>{project.id}</td>
+          <td>
+            {location.pathname === "/projects" ? (
+              <Link to={`/project/${project.id}`}>{project.projectName}</Link>
+            ) : (
+              <td>{project.projectName}</td>
+            )}
+          </td>
+          <td>{project.linkToRepo}</td>
+          <td>{project.description}</td>
+          <td>{project.projectTeam.map(id => {
+              const contributor = users.find(user => user.id === id);
+              return (!!contributor) ? <p key={id}>{contributor.email}</p> : <p>Loading...</p>;
+          })}
+          </td>
+          {location.pathname === "/projects" && (
+            // Кнопка "Delete" только на странице "/projects"
+            <td>
+              <button onClick={handleDelete}>Delete</button>
+            </td>
+          )}
+        </tr>
   );
 };
--- frontend/src/components/ProjectList.js
+++ frontend/src/components/ProjectList.js
@@ -1,23 +1,27 @@
 import ProjectItem from './Project';
 import Table from 'react-bootstrap/Table';
+import { NavLink } from "react-router-dom";
 
 export default function ProjectList ({ projects, users, setProjects }) {
   return (
-    <Table striped bordered hover>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Project Name</th>
-        <th>Link to repo</th>
-        <th>Description</th>
-        <th>Project Team</th>
-      </tr>
-    </thead>
-    <tbody>
-      {projects.map((project) => (
-        <ProjectItem project={project} users={users} setProjects={setProjects} />
-      ))}
-    </tbody>
-    </Table>
+      <div className="d-flex align-items-center flex-column">
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Project Name</th>
+            <th>Link to repo</th>
+            <th>Description</th>
+            <th>Project Team</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <ProjectItem project={project} users={users} setProjects={setProjects} />
+          ))}
+        </tbody>
+        </Table>
+        <NavLink to='create' className='btn btn-primary'>Create new</NavLink>
+      </div>
   );
 };
